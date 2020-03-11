f31f127005313b1ac7385f14fa5038f884ddda87
Ishan Virk <isv3@cornell.edu>
2020-03-11 15:35:37 -0400

    hehe

--- p2pApp.c
+++ p2pApp.c
@@ -2,3 +2,17 @@
 // Created by Will Krasnoff on 3/10/20.
 //
 
+
+
+
+void start_peers(int num_peers) {
+
+}
+
+int main(int argc, char **argv) {
+    num_peers = argv[1];
+    struct sockaddr_in server;
+
+    start_peers(num_peers)
+
+}
