0353a77b4a88ab095d734ee2d428656e0dd82c17
Ishan Virk <isv3@cornell.edu>
2020-03-17 02:08:24 -0400

    added udp coms

--- process_new.c
+++ process_new.c
@@ -6,20 +6,18 @@ int main(int argc , char *argv[])
     int pid = atoi(argv[1]);
     int num_procs = atoi(argv[2]);
     int tcp_port = atoi(argv[3]);
-    int udp_port = 20011 + pid;
+    int udp_port = 20000 + pid;
 
-    printf("TCP Port %d \n", tcp_port);
 
     struct sockaddr_in client_address;
     struct sockaddr_in tcp_address;
     struct sockaddr_in udp_address;
-    int tcp_socket, new_tcp_socket, udp_socket, i;
-    int opt = TRUE;
+    int tcp_socket, new_tcp_socket, udp_socket, valread, i;
+    char buffer[1025];
 
     fd_set active_fd_set;
     fd_set read_fd_set;
 
-    printf("In Process New\n");
     fflush(stdout);
 
     /*Create a TCP socket*/
@@ -36,23 +34,11 @@ int main(int argc , char *argv[])
         exit(EXIT_FAILURE);
     }
 
-    if((setsockopt(tcp_socket, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt))) < 0)
-    {
-        perror("Server-setsockopt() error");
-        close(tcp_socket);
-        exit (-1);
-    }
-    else
-        printf("Server-setsockopt() is OK\n");
-        fflush(stdout);
-
     /*Create TCP address */
     memset(&tcp_address, 0, sizeof(struct sockaddr_in));
     tcp_address.sin_family = AF_INET;
     tcp_address.sin_addr.s_addr = INADDR_ANY;
     tcp_address.sin_port = htons(tcp_port);
-
-    printf("Server added port %d to struct\n", ntohs(tcp_address.sin_port));
 
     /*Create UDP address */
     memset(&udp_address, 0, sizeof(struct sockaddr_in));
@@ -95,7 +81,7 @@ int main(int argc , char *argv[])
 
     /*send new connection greeting message_t*/
     if (send(new_tcp_socket, message, strlen(message), 0) != strlen(message)) {
-        perror("inital TCP connection send error");
+        perror("Intial TCP connection send error");
     }
 
     /*Bind the UDP socket to localhost and port with PID + 20000*/
@@ -112,6 +98,7 @@ int main(int argc , char *argv[])
     while(TRUE) {
         /* Block until input arrives on one or more active sockets. */
         read_fd_set = active_fd_set;
+
         if (select (FD_SETSIZE, &read_fd_set, NULL, NULL, NULL) < 0) {
             perror ("select");
             exit (EXIT_FAILURE);
@@ -120,16 +107,69 @@ int main(int argc , char *argv[])
         /* Service all the sockets with input pending. */
         for (i = 0; i < FD_SETSIZE; ++i) {
             if (FD_ISSET (i, &read_fd_set)) {
-                if ( i == new_tcp_socket){
-                    /* This is where we write stuff about proxy to server */
-                    printf("OMG GOT A TCP THAAAAANG\n");
+
+                /* PROXY -- SERVER TCP CONNECTION I/O */
+                if ( i == new_tcp_socket) {
+                    printf("Received a message on TCP\n");
                     fflush(stdout);
+
+                    /*Check if it was for closing , and also read the incoming message_t*/
+                    if ((valread = read( new_tcp_socket , buffer, 1024)) == 0) {
+                        /*Somebody disconnected , get his details and print*/
+                        int client_addr_len = sizeof(client_address);
+                        getpeername(new_tcp_socket , (struct sockaddr*)&client_address , (socklen_t *) &client_addr_len);
+                        printf("Host disconnected , ip %s , port %d \n" , inet_ntoa(client_address.sin_addr) , ntohs(client_address.sin_port));
+                        close( new_tcp_socket );
+                    }
+
+                    /*Echo back the message_t that came in*/
+                    else {
+                        buffer[valread] = '\0';
+                        send(new_tcp_socket , buffer , strlen(buffer) , 0 );
+                    }
+                    /* Send new message to nearby server */
+                    //TODO: Make all the below stuff into a function under gossip stuff
+                    char *test_msg = "This message should be sent over UDP";
+                    struct sockaddr_in test_serv_addr;
+                    test_serv_addr.sin_family = AF_INET;
+                    test_serv_addr.sin_addr.s_addr = INADDR_ANY;
+                    int test_serv_port = get_open_neighbors(pid);
+                    test_serv_addr.sin_port =htons(test_serv_port);
+
+                    sendto(udp_socket, (const char *)test_msg, strlen(test_msg), MSG_DONTWAIT, (const struct sockaddr *) &test_serv_addr, sizeof(test_serv_addr));
+                    printf("Test message sent on UDP.\n");
+
+                /* SERVER -- SERVER UDP CONNECTION I/O */
                 } else {
-                    /* This is where we write stuff about proxy to server */
-                    printf("DAMN WE GOT A DIF THANG\n");
+                    printf("Received a message on UDP\n");
                     fflush(stdout);
+
+                    //TODO: Put all this code in proper gossip logic functions
+                    struct sockaddr_in peer_serv_addr;
+                    memset(&peer_serv_addr, 0, sizeof(peer_serv_addr));
+                    char udp_buffer[1024];
+                    int len, n;
+
+                    len = sizeof(peer_serv_addr);
+
+                    if (n = recvfrom(udp_socket, (char *)udp_buffer, 1024, MSG_DONTWAIT, ( struct sockaddr *) &peer_serv_addr, &len) == -1) {
+                        perror("Recv From Failed\n");
+                    }
+
+                    udp_buffer[n] = '\0';
+                    printf("Client : %s\n", udp_buffer);
                 }
             }
         }
+    }
+}
+
+int get_open_neighbors(int pid) {
+    //TODO: Write proper logic here to find nearby neighbours
+    //For now just writing test code
+    if (pid == 1) {
+        return 20002;
+    } else {
+        return 20001;
     }
 }
